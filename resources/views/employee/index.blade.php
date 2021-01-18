35e9523fc40edc3e0fadd059df7c468ecf103628
ManuchehrM <muzafarov9797@gmail.com>
2021-01-18 11:39:37 +0500

    crud employees complated
    crud employees complated

---
+++ resources/views/employee/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layout')
+
+@section('content')
+
+<div class="mt-5">
+<a class="btn btn-info mb-1" href="{{ route('employees.create') }}">Add Employee</a><br> 
+  @if(session()->get('success'))
+    <div class="alert alert-success">
+      {{ session()->get('success') }} 
+    </div>
+  @endif
+  <table class="table">
+    <thead>
+        <tr class="table-success">
+          <td># ID</td>
+          <td>Photo</td>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Phone</td>
+          <td>Action</td>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($employee as $employee)
+        <tr>
+            <td>{{$employee->id}}</td>
+            <td><img src="{{asset('images').'/'.$employee->avatar}}" width="80px"></td>
+            <td>{{$employee->name}}</td>
+            <td>{{$employee->email}}</td>
+            <td>{{$employee->phone}}</td>
+            <td class="text-center">
+                <a href="{{ route('employees.edit', $employee->id)}}" class="btn btn-success btn-sm">Edit</a>
+                <form action="{{ route('employees.destroy', $employee->id)}}" method="post" style="display: inline-block">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger btn-sm" type="submit">Delete</button>
+                  </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+  </table>
+<div>
+@endsection
